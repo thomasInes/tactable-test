2c5687397879edabde9712e22350b252c3147739
INES Thomas <tines@bimm.com>
2023-01-21 17:12:16 -0500

    init typescript express app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tactable-test
-small API using Typescript and inversifyJS
+## Tactable technical test
+instructions in [backend-code-challenge.md](./backend-code-challenge.md)
+
+## Installation
+```bash
+npm install
+```
+
+## Run the project
+```bash
+npm run dev
+```
+then go to https://localhost:3000 
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+const app: Application = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  });
+});
+
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
